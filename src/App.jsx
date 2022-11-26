19cecf67612c0c01ec28dd8c2cc46146cb7c1d65
Avinash Patel <avinashpatel9755@gmail.com>
2022-11-26 13:37:41 +0530

    Adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
+import SocialLinks from "./Components/SocialLinks";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <SocialLinks />
     </div>
   );
 }
