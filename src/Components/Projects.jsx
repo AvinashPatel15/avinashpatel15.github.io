698f317ce207ecd0fb6ea3d5b77dfe9b12ab112f
Avinash Patel <avinashpatel9755@gmail.com>
2023-03-02 13:54:00 +0530

    updated Projects Page

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,16 +3,37 @@ import mailchimpclone from "../Assets/mailchimpclone.png";
 import youtubeclone from "../Assets/youtubeclone.png";
 import bestbuyclone from "../Assets/bestbuyclone.png";
 import awakefashion from "../Assets/awakefashion.png";
+import newwavefashion from "../Assets/newwavefashion.png";
 import { IoLogoJavascript } from "react-icons/io";
 import { FaCss3Alt, FaReact } from "react-icons/fa";
 import { RiHtml5Line } from "react-icons/ri";
-import { SiChakraui, SiMaterialui } from "react-icons/si";
+import { SiChakraui, SiExpress, SiMaterialui, SiMongodb } from "react-icons/si";
 import { BsBootstrapFill } from "react-icons/bs";
+import { GrNode } from "react-icons/gr";
 
 const Projects = () => {
   const project = [
     {
       id: 1,
+      src: newwavefashion,
+      demo: "https://new-wave-fashion.vercel.app/",
+      code: "https://github.com/AvinashPatel15/NewWave-Fashion",
+      projName: "NewWave Fashion",
+      Description:
+        "NewWave ultimate fashion destination for styles that are handpicked, on trend and at prices.",
+      TechStack: [
+        { stack: <RiHtml5Line size={30} /> },
+        { stack: <FaCss3Alt size={30} /> },
+        { stack: <IoLogoJavascript size={30} /> },
+        { stack: <FaReact size={30} /> },
+        { stack: <SiChakraui size={30} /> },
+        { stack: <GrNode size={30} /> },
+        { stack: <SiExpress size={30} /> },
+        { stack: <SiMongodb size={30} /> },
+      ],
+    },
+    {
+      id: 2,
       src: youtubeclone,
       demo: "https://youtube-clone-react-pi.vercel.app/",
       code: "https://github.com/AvinashPatel15/Youtube-Clone-React",
@@ -28,7 +49,7 @@ const Projects = () => {
       ],
     },
     {
-      id: 2,
+      id: 3,
       src: mailchimpclone,
       demo: "https://mailchimp-clone-aries.netlify.app",
       code: "https://github.com/AvinashPatel15/Mail-Chimp-Clone",
@@ -44,7 +65,7 @@ const Projects = () => {
       ],
     },
     {
-      id: 3,
+      id: 4,
       src: awakefashion,
       demo: "https://awake-fashion.netlify.app/",
       code: "https://github.com/AvinashPatel15/Awake-Fashion-ReactJS",
@@ -60,7 +81,7 @@ const Projects = () => {
       ],
     },
     {
-      id: 4,
+      id: 5,
       src: bestbuyclone,
       demo: "https://best-buy-clone-aries.netlify.app",
       code: "https://github.com/AvinashPatel15/Best-Buy-Clone",
@@ -110,9 +131,7 @@ const Projects = () => {
                   <div className="font-bold"> Tech-Stack:</div>
                   <p className="grid grid-cols-5 gap-2 justify-center items-center mt-2">
                     {TechStack.map((el) => (
-                      <span class="cursor-pointer">
-                        {el.stack}
-                      </span>
+                      <span class="cursor-pointer">{el.stack}</span>
                     ))}
                     {/* <span class="text-black-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:text-black-400 border-2 border-purple-700">
                       Purple
